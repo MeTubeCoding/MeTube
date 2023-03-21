839d849e8ec95f6c4fce270144eafa62cb07f7d5
Ramjoh <romjah@orange.fr>
2023-03-21 17:30:20 +0100

    youpi

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+// import composant  :
+console.log("me voit tu ?")
+
+let test = document.getElementById("test");
+
+test?.addEventListener("click",()=>{
+  fetch('http://localhost:5600/demo')
+    // .then(response => response.text())
+    // .then(data => console.log(data))
+    // .catch(error => console.error(error));
+});
+
+console.log("me voit tu ? 2")
+
+function App() {
+  return (
+    <div>test</div>
+  )
+}
+
+export default App;
