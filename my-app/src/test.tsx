2ac17e9d88df46c990fc3f8d6513f6b7d75b51ec
Ramjoh <romjah@orange.fr>
2023-03-21 13:16:28 +0100

    icones

--- my-app/src/test.tsx
+++ my-app/src/test.tsx
@@ -1,4 +1,3 @@
-
 // Vos futur composant ou logique de programmation: 
 
 export function example():void{
@@ -6,6 +5,48 @@ export function example():void{
     console.log("je suis une fonction ou un futur composant")
 }
 
+/*
+
+_______________________ Shorts ______________________
 
 
+<FontAwesomeIcon icon="fa-light fa-heart" style={{color: "#ffffff",}} />  //coeur vide blanc
 
+<FontAwesomeIcon icon="fa-solid fa-heart" beat style={{color: "#f20202",}} /> //coeur plein avec anime rouge
+
+<FontAwesomeIcon icon="fa-solid fa-heart-crack" style={{color: "#ffffff",}} /> //coeur vide brisé blanc
+
+<FontAwesomeIcon icon="fa-solid fa-heart-crack" shake style={{color: "#f",}} /> //coeur plein brisé avec anime rouge
+
+<FontAwesomeIcon icon="fa-solid fa-message" style={{color: "#ffffff",}} /> // commentaire plein en blanc
+
+<FontAwesomeIcon icon="fa-solid fa-paper-plane" style={{color: "#ffffff",}} /> // fleche d'envoie d'un message plein en blanc
+
+<FontAwesomeIcon icon="fa-sharp fa-solid fa-bookmark" style={{color: "#ffffff",}} /> // favoris plein en blanc
+
+<FontAwesomeIcon icon="fa-sharp fa-solid fa-share" style={{color: "#ffffff",}} /> //la fleche du partage pleine en blanc
+
+<FontAwesomeIcon icon="fa-sharp fa-solid fa-play" style={{color: "#f1efef",}} /> //triangle du reprendre en grisé
+
+<FontAwesomeIcon icon="fa-sharp fa-solid fa-pause" style={{color: "#f1efef",}} /> //les barre du pause en grisé
+
+<FontAwesomeIcon icon="fa-regular fa-bars" style={{color: "#ffffff",}} /> //Les barres du menu de description
+
+<FontAwesomeIcon icon="fa-solid fa-ban" rotation={90} style={{color: "#ffffff",}} /> // ne pas recommandé en blanc
+
+<FontAwesomeIcon icon="fa-sharp fa-regular fa-flag" style={{color: "#f1f2f4",}} /> // le drapeau pour signaler en grisé
+
+<FontAwesomeIcon icon="fa-sharp fa-regular fa-message-exclamation" style={{color: "#ebecf0",}} /> // envoyer des commentaires à youtube en grisé
+
+<FontAwesomeIcon icon="fa-solid fa-period" style={{color: "#e8eaed",}} /> // point pour l'icone paramètres
+
+<FontAwesomeIcon icon="fa-sharp fa-solid fa-gear" style={{color: "#e9eaec",}} /> // engrenage pour les paramètres
+
+
+_______________________ Acceuil ______________________
+
+<FontAwesomeIcon icon="fa-solid fa-chevron-left" rotation={270} style={{color: "#e7eaee",}} /> // Le voir plus des shorts
+<FontAwesomeIcon icon="fa-sharp fa-regular fa-xmark" style={{color: "#e8eaed",}} /> // croix pour supprimer l'onglet pour les shorts dans l'acceuil
+
+
+*/
