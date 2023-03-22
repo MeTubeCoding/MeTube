2de176640ca0495b34306f7a7c73f0ad53476e49
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-03-22 15:40:01 +0100

    bdd

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -14,7 +14,6 @@ export default function Signup(){
   const handleChange=(e)=>setSignupState({...signupState,[e.target.id]:e.target.value});
 
   const handleSubmit=(e)=>{
-    console.log("qsdfghjklm");
     e.preventDefault();
     console.log(signupState)
     createAccount()
