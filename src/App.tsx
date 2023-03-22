ffdc217030833f034308dc34e43644f6846e2e80
Stanewolfer <seiya0500@gmail.com>
2023-03-22 09:30:55 +0100

    page test

--- src/App.tsx
+++ src/App.tsx
@@ -23,24 +23,12 @@ test?.addEventListener("click",()=>{
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div id='test'>Test</div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <body>
+      <div className="header">
+        <h1>MeTUNE</h1>
+      </div>
+      </body>
+  )
 }
 
 export default App;
