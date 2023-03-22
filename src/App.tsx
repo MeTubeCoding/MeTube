11f24aaa028f1ba77abf9a975a71842975e462f7
Ramjoh <romjah@orange.fr>
2023-03-22 15:25:54 +0100

    pushhooooo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-// import React from 'react';
-import './App.css';
+import React from 'react';
+import './App.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHome, faHeart, faHeartBroken, faPaperclip, faVolumeUp, faBookmark, faPaperPlane, faPlay, faPause, faForward, faForwardFast} from '@fortawesome/free-solid-svg-icons';
+import {faChevronDown} from '@fortawesome/free-solid-svg-icons';
+
 
 // import composant  :
 console.log("me voit tu ?")
@@ -8,18 +12,31 @@ let test = document.getElementById("test");
 
 test?.addEventListener("click",()=>{
   fetch('http://localhost:5600/demo')
-    // .then(response => response.text())
-    // .then(data => console.log(data))
-    // .catch(error => console.error(error));
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
 });
 
 
 function App() {
   return (
-    <div className="App">
-      <div>test</div>
-    </div>
+    <div className='App bg-gray-700 display:flex align-items:space-between'>
+      <h1 className="short text-white">SHORT</h1>
+        <FontAwesomeIcon icon={faHome} />
+        <FontAwesomeIcon icon={faHeart} style={{color: "#e70808",}} />
+        <FontAwesomeIcon icon={faHeartBroken} style={{color: "#f9fafb",}} />
+        <FontAwesomeIcon icon={faPaperclip} style={{color: "#ebecf0",}} />
+        <FontAwesomeIcon icon={faVolumeUp} style={{color: "#ebecf0",}} />
+        <FontAwesomeIcon icon={faBookmark} style={{color: "#ebecf0",}} />
+        <FontAwesomeIcon icon={faPaperPlane} style={{color: "#ebecf0",}} />
+        <FontAwesomeIcon icon={faPlay} style={{color: "#ebecf0",}} />
+        <FontAwesomeIcon icon={faPause} style={{color: "#ebecf0",}} /> 
+        <FontAwesomeIcon icon={faForward} style={{color: "#ebecf0",}} />
+        <FontAwesomeIcon icon={faForwardFast} style={{color: "#ebecf0",}} />
+        <FontAwesomeIcon icon={faChevronDown} style={{color: "#ebecf0",}} />
+    </div> 
   )
 }
+
 
 export default App;
