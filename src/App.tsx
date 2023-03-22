f3befa65c3e8ef62659c127cad6a6fbe3e3bb286
Baptiste GRIMALDI <100611587+BaptisteGrimaldi@users.noreply.github.com>
2023-03-22 10:23:04 +0100

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -22,24 +21,12 @@ test?.addEventListener("click",()=>{
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div id='test'>Test</div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Je suis un test</p>
+      <p>Je suis du texte</p>
+    </>
   );
 }
+
 
 export default App;
