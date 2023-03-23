1a47dd006cb960afffc529bc084b16267ef22d8e
leo <leoyng95@gmail.com>
2023-03-23 11:26:08 +0100

    description

---
+++ src/composant/description.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function Description(){
+    // state (état, données)
+
+    // comportements 
+
+
+    // affichage (render)
+
+    
+    return( 
+        <>
+
+
+        <p>description </p>
+
+        </>
+        )
+
+}
