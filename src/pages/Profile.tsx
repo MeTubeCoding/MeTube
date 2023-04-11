1b3a7307a1f2b8c9a4bb1f7eeba41dc0e66f8484
Turbotard <turbotard9@gmail.com>
2023-04-11 14:46:12 +0200

    small correction

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import Header from '../components/Header'
