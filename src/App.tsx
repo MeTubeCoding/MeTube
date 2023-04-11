72bf5bb2a58252a4a4d77f13e32afa9ac7811610
Sacha <107134875+xivsacha@users.noreply.github.com>
2023-04-11 14:54:22 +0200

    update 2 files and delete 1 file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import './App.css'
 
+
+
+
+
+
 const test = document.getElementById('test')
 
 test?.addEventListener('click', () => {
@@ -9,7 +14,6 @@ test?.addEventListener('click', () => {
     // .then(data => console.log(data))
     // .catch(error => console.error(error));
 });
-
 
 
 const Menu = () => {
@@ -30,9 +34,9 @@ const Menu = () => {
           <li>
             <div className="menu-button" onClick={toggleMenu}>
               {isMenuOpen ? (
-                <button className="material-icons">Close</button>
+                <button className="material-icons bg-neutral-700 p-5 rounded-lg active:bg-neutral-800">Close</button>
               ) : (
-                <button className="material-icons">Menu</button>
+                <button className="material-icons bg-neutral-700 p-5 rounded-lg active:bg-neutral-800">Menu</button>
               )}
             </div>
             {isMenuOpen && (
@@ -41,19 +45,19 @@ const Menu = () => {
                   <a href="index.tsx">Accueil</a>
                 </li>
                 <li>
-                  <a href="pages/timeline.tsx">Recommandés</a>
+                  <a href="pages/timeline.tsx" className='bg-neutral-700 p-5 rounded-lg active:bg-neutral-800'>Recommandés</a>
                 </li>
                 <li>
-                  <a href="#">Tendances</a>
+                  <a href="#" className='bg-neutral-700 p-5 rounded-lg active:bg-neutral-800'>Tendances</a>
                 </li>
                 <li>
-                  <a href="#">Abonnements</a>
+                  <a href="#" className='bg-neutral-700 p-5 rounded-lg active:bg-neutral-800'>Abonnements</a>
                 </li>
                 <li>
-                  <a href="#">Bibliothèque</a>
+                  <a href="#" className='bg-neutral-700 p-5 rounded-lg active:bg-neutral-800'>Bibliothèque</a>
                 </li>
                 <li>
-                  <a href="#">Historique</a>
+                  <a href="#" className='bg-neutral-700 p-5 rounded-lg active:bg-neutral-800'>Historique</a>
                 </li>
               </div>
             )}
@@ -77,18 +81,18 @@ const Menu = () => {
 function App() {
   return (
     <div>
-      <body>
-        <div className = "haut">
-          <a href='App.tsx'><h1>MeTUBE</h1></a>
-          <div className="search">
-            <input type="text" placeholder="Rechercher sur MeTube..." className='Searching' />
-            <button className='rechercher'>Rechercher</button>
+      <div className="bg-neutral-900 text-white font-roboto text-base font-normal leading-6 text-left m-0 p-0">
+        <div className="flex justify-between items-center py-4 px-6">
+          <a href='App.tsx' className="text-white font-bold text-2xl">MeTUBE</a>
+          <div className="flex items-center">
+            <input type="text" placeholder="Rechercher sur MeTube..." className='px-2 py-1 rounded-lg text-black mr-2' />
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg'>Rechercher</button>
           </div>
         </div>
         <div>
           {Menu()}
         </div>
-      </body>
+      </div>
     </div>
   )
 }
