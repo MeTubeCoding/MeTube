ddcd0da25fd483d50347416bbd92754cbe579c80
Tristan <turgytristan@gmail.com>
2023-04-11 13:57:12 +0200

    fix caméra

--- src/composant/Live.tsx
+++ src/composant/Live.tsx
@@ -1,32 +1,31 @@
 import React from 'react';
+import './Live.css'; // Importez le fichier CSS
 
+export function Live() {
 
-export function Live(){
+  let localStream: MediaStream;
+  let remoteStream;
 
-    let localStream: MediaStream;
-    let remoteStream;
-    
-    const init = async () => {
-    
-      try {
-        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-      } catch(error) {
-        // Gérer l'erreur ici
-      }
-      
-      (document.getElementById('user-1') as HTMLVideoElement).srcObject = localStream;
+  const init = async () => {
+
+    try {
+      localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    } catch (error) {
+      // Gérer l'erreur ici
     }
 
-    init();
+    (document.getElementById('user-1') as HTMLVideoElement).srcObject = localStream;
+  }
 
-    return(
+  init();
+
+  return (
     <>
-    <div id="videos" className='border flex justify-around'>
-        <video className="video-player" id="user-1" autoPlay playsInline></video>
+      <div id="videos" className='border flex justify-around'>
+        <video className="video-player flipped" id="user-1" autoPlay playsInline></video>
         <video className="video-player" id="user-2" autoPlay playsInline></video>
-    </div>
-    </>   
+      </div>
+    </>
 
-        
-    )
+  )
 }
