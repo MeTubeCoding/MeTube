8132caa3a2bf06e0448fde7ea3a859663564a5fd
Romjah <romjah@orange.com>
2023-04-11 19:25:41 +0200

    push

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Main from './pages/editeur'
+import Editeur from './pages/Editeur'
 
 function App () {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Main />} />
+          <Route path='/' element={<Editeur />} />
 
           <Route path='/:id' element={<h1>Page Résultats</h1>} />
         </Routes>
--- src/pages/editeur.tsx
+++ src/pages/editeur.tsx
@@ -1,37 +1,12 @@
+/* eslint-disable @typescript-eslint/indent */
 /* eslint-disable eol-last */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React, { useEffect, useState } from 'react'
-import SearchBar from '../components/SearchBar'
-import Results, { type IResults } from '../components/Results'
-
-const Main = () => {
-  const [videos, setVideos] = useState<IResults | []>([])
-  useEffect(() => {
-
-  }, [])
-  const [searchDatas, setSearchDatas] = useState('')
-
-  const onSearch = (data: string) => {
-    console.log(JSON.stringify(data))
-
-    fetch('http://localhost:5600/videos',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ data })
-      }
-    ).then(async response => await response.json()).then((response: IResults) => { setVideos(response) })
+function Editeur () {
+    return (
+        <div>yo</div>
+    )
   }
-  return (
-    <div>
-      <SearchBar onSearch={onSearch}/>
-      <Results videos={videos}/>
-    </div>
-  )
-}
-
-export default Main
+export default Editeur
