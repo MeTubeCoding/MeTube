901a7f22d88ffa2ba7a519c4dbeee94e2043fda0
Tristan Turgy <116552625+ArKZbeb@users.noreply.github.com>
2023-04-11 11:41:50 +0200

    mege final

--- src/composant/chat.tsx
+++ src/composant/chat.tsx
@@ -1,34 +1,3 @@
-export function Chat() {
-
-  function getMessageChat(){
-    fetch("http://127.0.0.1:5600/chat",{
-      method : "GET",
-      })
-  }
-
-    return (
-      <>
-        <div>
-          <p id="titre_chat" className="text-lg font-bold text-blue-500">
-            Espace de modération
-          </p>
-          <button className="bg-blue-500 text-white font-bold py-1 px-2 rounded">test</button>
-        </div>
-        <div
-          style={{
-            width: '30%',
-            height: '600px',
-            backgroundColor: '#grey',
-            border: '1px solid #ccc',
-            borderRadius: '20px',
-            marginTop: '20px',
-            marginLeft: '10px',
-          }}
-        ></div>
-      </>
-    );
-  }
-  
 import React from 'react';
 export function Chat(){
 
@@ -75,19 +44,33 @@ export function Chat(){
     return(
       <>
         <div>
-          
+          <p id="titre_chat" className="text-lg font-bold text-blue-500">
+            Espace de modération
+          </p>
+          <button className="bg-blue-500 text-white font-bold py-1 px-2 rounded">test</button>
         </div>
-
-        <div className='height: 300px;overflow-y: scroll;border: 1px solid #ccc;padding: 10px;'>
-          
-          <form className='margin-top: 10px;'>
-            <label htmlFor="message-input">Message:</label>
-            <input type="text" name="message" id="message-input"className='width: 80%;padding: 10px;border: 1px solid #ccc;border-radius: 3px;'></input>
-            <p onClick={log}>send</p>
-
-            <p onClick={getChat}>chat</p>
-          </form>
-        </div>
+        <div
+          style={{
+            width: '30%',
+            height: '600px',
+            backgroundColor: '#grey',
+            border: '1px solid #ccc',
+            borderRadius: '20px',
+            marginTop: '20px',
+            marginLeft: '10px',
+          }}
+        ></div>
+  
+          <div className='height: 300px;overflow-y: scroll;border: 1px solid #ccc;padding: 10px;'>
+            
+            <form className='margin-top: 10px;'>
+              <label htmlFor="message-input">Message:</label>
+              <input type="text" name="message" id="message-input"className='width: 80%;padding: 10px;border: 1px solid #ccc;border-radius: 3px;'></input>
+              <p onClick={log}>send</p>
+  
+              <p onClick={getChat}>chat</p>
+            </form>
+          </div>
         </>
     )
 }
