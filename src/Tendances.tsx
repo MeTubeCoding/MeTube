7ae14db9518be983c60eb2b290c272c374ba81e9
Stanewolfer <seiya0500@gmail.com>
2023-04-11 15:06:21 +0200

    tailwind à peu près fonctionnel

--- src/Tendances.tsx
+++ src/Tendances.tsx
@@ -8,44 +8,50 @@ import './Tendances.css';
 import { PassThrough } from 'stream';
 
 const Menu = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen(!isMenuOpen)
   };
 
   const closeMenu = () => {
-    setIsMenuOpen(false);
+    setIsMenuOpen(false)
   };
 
   return (
     <div className="menu-container">
-      <div className="menu-button" onClick={toggleMenu}>
-        {isMenuOpen ? (
-          <button className="material-icons">Close</button>
-        ) : (
-          <button className="material-icons">Menu</button>
-        )}
-      </div>
       <div className={`menu ${isMenuOpen ? 'open' : ''} texts`}>
         <ul>
           <li>
-            <a href="index.tsx">Accueil</a>
-          </li>
-          <li>
-            <a href="pages/timeline.tsx">Recommandés</a>
-          </li>
-          <li>
-            <a href="#">Tendances</a>
-          </li>
-          <li>
-            <a href="#">Abonnements</a>
-          </li>
-          <li>
-            <a href="#">Bibliothèque</a>
-          </li>
-          <li>
-            <a href="#">Historique</a>
+            <div className="menu-button" onClick={toggleMenu}>
+              {isMenuOpen ? (
+                <button className="material-icons">Close</button>
+              ) : (
+                <button className="material-icons">Menu</button>
+              )}
+            </div>
+            {isMenuOpen && (
+              <div>
+               <li>
+                  <a href="index.tsx">Accueil</a>
+                </li>
+                <li>
+                  <a href="pages/timeline.tsx">Recommandés</a>
+                </li>
+                <li>
+                  <a href="#">Tendances</a>
+                </li>
+                <li>
+                  <a href="#">Abonnements</a>
+                </li>
+                <li>
+                  <a href="#">Bibliothèque</a>
+                </li>
+                <li>
+                  <a href="#">Historique</a>
+                </li>
+              </div>
+            )}
           </li>
         </ul>
       </div>
@@ -64,29 +70,29 @@ const Menu = () => {
 
 function Tendances() {
 return (
-    <div>
-      <div className="haut">
-        <div className='haut_gauche'>
+    <div className = "bg-gray-900 text-white font-sans font-normal text-base leading-relaxed text-left m-0 p-0 h-screen">
+      <div className="text-center flex flex-row bg-gray-800 text-white p-4 flex justify-around items-center">
+        <div className='flex flex-row justify-between items-center mr-auto'>
         <div className='titre'>
-        <a href='App.tsx'><img className="logo" src="https://cdn.discordapp.com/attachments/1019222515962368112/1088482444207988838/Metube_logo.png"/></a>
+        <a href='App.tsx'><img className="mr-10 w-1/2" src="https://cdn.discordapp.com/attachments/1019222515962368112/1088482444207988838/Metube_logo.png"/></a>
         </div>
         </div>
-        <div className="search">
-          <input type="text" placeholder="Rechercher sur MeTube..." className='Searching' />
-          <button className='rechercher'>Rechercher</button>
+        <div className="flex items-center justify-between mr-10 bg-gray-800">
+          <input type="text" placeholder="Rechercher sur MeTube..." className='w:90 pl-1 rounded-2xl border border-gray-600 bg-gray-800 hover:bg-gray-700 text-white placeholder:italic' />
+          <button className='bg-gray-600 border-none rounded-2xl px-4 py-2 mr-1 text-white hover:bg-gray-700'>Rechercher</button>
         </div>
       </div>
       <div>
           {Menu()}
         </div>
-      <div className="tendances">
-        <img className='img_tendances' src="https://www.youtube.com/img/trending/avatar/trending.png"></img>
-        <h3>TENDANCES</h3>
+      <div className="flex flex-row justify-center items-center my-3 mx-1/100 lg:mx-5/100">
+        <img className='rounded-full mr-2%' src="https://www.youtube.com/img/trending/avatar/trending.png"></img>
+        <h3 className='m-0 p-0 text-center leading-normal text-3xl'>TENDANCES</h3>
         </div>
-      <div className="filtres">
-        <div className="nv">
+      <div className="flex items-center justify-center">
+        <div className="ml-3.5% mr-5/100 p-0 text-center flex flex-col">
         <h4>NOUVEAUTES</h4>
-        <iframe
+        <iframe className='mr-1% rounded-lg'
             width="300"
             height="170"
             src="https://www.youtube.com/embed/SRpYxmmy6EI"
@@ -95,7 +101,7 @@ return (
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
-          <iframe
+          <iframe className='mr-1% rounded-lg'
             width="300"
             height="170"
             src="https://www.youtube.com/embed/dMekskqXbL4"
@@ -105,9 +111,9 @@ return (
             allowFullScreen
           ></iframe>
         </div>
-        <div className="music">
+        <div className="mr-5/100 p-0 text-center flex flex-col">
         <h4>MUSIQUE</h4>
-        <iframe
+        <iframe className='mr-1% rounded-lg'
             width="300"
             height="170"
             src="https://www.youtube.com/embed/dQw4w9WgXcQ"
@@ -116,7 +122,7 @@ return (
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
-           <iframe
+           <iframe  className='mr-1% rounded-lg'
             width="300"
             height="170"
             src="https://www.youtube.com/embed/3NoKAOTE_ZI"
@@ -126,9 +132,9 @@ return (
             allowFullScreen
           ></iframe>
         </div>
-        <div className="films">
+        <div className="mr-5/100 p-0 text-center flex flex-col">
         <h4>FILMS</h4>
-        <iframe
+        <iframe   className='mr-1% rounded-lg'
             width="300"
             height="170"
             src="https://www.youtube.com/embed/RjNcTBXTk4I"
@@ -137,7 +143,7 @@ return (
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
-           <iframe
+           <iframe  className='mr-1% rounded-lg'
             width="300"
             height="170"
             src="https://www.youtube.com/embed/wK69QJ2ESO0"
@@ -147,9 +153,9 @@ return (
             allowFullScreen
           ></iframe>
         </div>
-        <div className="jeux">
+        <div className="mr-5/100 p-0 text-center flex flex-col">
         <h4>JEUX VIDEOS</h4>
-        <iframe
+        <iframe  className='mr-1% rounded-lg'
             width="300"
             height="170"
             src="https://www.youtube.com/embed/loarI5X93wI"
@@ -158,7 +164,7 @@ return (
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
-          <iframe
+          <iframe className='mr-1% rounded-lg'
             width="300"
             height="170"
             src="https://www.youtube.com/embed/QBBLxrvOmqw"
