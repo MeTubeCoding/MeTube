01832265acc0dbd5bce989e3ee1db1c72a12e632
Tristan <turgytristan@gmail.com>
2023-04-11 09:24:32 +0200

    chat

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,10 @@ import './App.css';
 import { Chat } from './composant/chat';
 
 
-function log(){
-  console.log("log")
-  let local = {
-    "mdp":"test" ,
-    "gmail": "mail",
-
-  };
- 
-  fetch("http://127.0.0.1:5600/chat",{
- 
-  method : "POST",
-  headers : {
-      "Content-Type" : "application/json"
-  },
-  body : JSON.stringify(local)
-  })
-  .then((res)=>{
-  return res.text()}
-  )
-  .then((res)=>{
-    console.log(res)
-  })
-  
-
-
-}
-
 
 function App() {
   return (
     <>
-      <p onClick={log}>Je suis un test</p>
       <p className="text-3xl font-bold text-red-500">Je suis du texte</p>
       <Chat/>
     </>
