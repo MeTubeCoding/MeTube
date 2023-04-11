3927c8b6dcb50c0d2cd4f72235c6d521a09b5da0
Romjah <romjah@orange.com>
2023-04-11 23:19:58 +0200

    dodo ^^

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -23,11 +23,12 @@ const NavigationBar = () => {
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link
-                  to="/video"
+                  to="/pages/VideoPage.tsx"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Video Editor
                 </Link>
+
                 <Link
                   to="/music"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
---
+++ src/pages/VideoPage.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/space-before-function-paren */
+import React from 'react'
+import VideoEditor from '../components/VideoEditor'
+
+function VideoPage() {
+  return (
+    <div>
+      <VideoEditor />
+    </div>
+  )
+}
+
+export default VideoPage
