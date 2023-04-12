9c6e1c13c9eaa7664502d76e19536cdaba0ce88e
YimoSsDev <116626946+YimoSsDev@users.noreply.github.com>
2023-04-12 09:13:39 +0200

    update 1.3

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable multiline-ternary */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+
 import React, { useState } from 'react'
 import VideoItem from '../components/VideoItem'
 import NavigationBar from '../components/NavigationBar'
@@ -15,16 +16,18 @@ const HomePage = () => {
   }
 
   return (
-    <div className="home-page">
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100px' }}>
       <NavigationBar />
-      {selectedVideo ? (
-        <VideoItem video={selectedVideo} />
-      ) : (
-        <div>
-          <h2>Select a video to get started:</h2>
-          <input type="file" accept="video/*" onChange={handleVideoUpload} />
-        </div>
-      )}
+      <div style={{ display: 'flex', flexDirection: 'column-reverse', alignItems: 'center' }}>
+        {selectedVideo ? (
+          <VideoItem video={selectedVideo} />
+        ) : (
+          <div>
+            <h2>Select a video to get started:</h2>
+            <input type="file" accept="video/*" onChange={handleVideoUpload} />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
