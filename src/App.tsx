756e4b792fa90c8e60fca4c59781f8a7ccafe4c8
Sacha <107134875+xivsacha@users.noreply.github.com>
2023-04-12 16:10:59 +0200

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes as Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 
 
