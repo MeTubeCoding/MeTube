eed22510406f765e859f5f2c82632660e079de63
Sacha <107134875+xivsacha@users.noreply.github.com>
2023-04-12 09:27:07 +0200

    update App.tsx and src/index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,12 +34,12 @@ const Menu = () => {
           <li>
             <div className="menu-button" onClick={toggleMenu}>
               {isMenuOpen ? (
-                <div> 
+                <div className='ml-10 md-6'> 
                   <button className="material-icons hover:bg-neutral-700 w-10 h-10 rounded-lg active:bg-neutral-800">☰</button>
                   <p>Close</p>
                 </div>
               ) : (
-                <div>
+                <div className='ml-5 md-6'>
                   <button className="material-icons hover:bg-neutral-700 w-10 h-10 rounded-lg active:bg-neutral-800">☰</button>
                   <p>Menu</p>
                 </div>
@@ -47,22 +47,22 @@ const Menu = () => {
             </div>
             {isMenuOpen && (
               <div>
-                <li className='hover:bg-neutral-700 w-10 h-10 rounded-lg active:bg-neutral-800'>
-                  <a href="index.tsx">⌂</a>
+                <li className='material-icons hover:bg-neutral-700 p-3 rounded-lg active:bg-neutral-800'>
+                  <a href="index.tsx" className='text-align:center'>Accueil</a>
                 </li>
-                <li className='hover:bg-neutral-700 w-10 h-10 rounded-lg active:bg-neutral-800'>
-                  <a href="pages/timeline.tsx">🔥</a>
+                <li className='hover:bg-neutral-700 p-3 rounded-lg active:bg-neutral-800'>
+                  <a href="pages/timeline.tsx">Recommandations</a>
                 </li>
-                <li className='hover:bg-neutral-700 w-10 h-10 rounded-lg active:bg-neutral-800'>
+                <li className='hover:bg-neutral-700 p-3 rounded-lg active:bg-neutral-800'>
                   <a href="#" >Tendances</a>
                 </li>
-                <li className='hover:bg-neutral-700 w-10 h-10 rounded-lg active:bg-neutral-800'>
+                <li className='hover:bg-neutral-700 p-3 rounded-lg active:bg-neutral-800'>
                   <a href="#" >Abonnements</a>
                 </li>
-                <li className='hover:bg-neutral-700 w-10 h-10 rounded-lg active:bg-neutral-800'>
+                <li className='hover:bg-neutral-700 p-3 rounded-lg active:bg-neutral-800'>
                   <a href="#" >Bibliothèque</a>
                 </li>
-                <li className='hover:bg-neutral-700 w-10 h-10 rounded-lg active:bg-neutral-800'>
+                <li className='hover:bg-neutral-700 p-3 rounded-lg active:bg-neutral-800'>
                   <a href="#" >Historique</a>
                 </li>
               </div>
@@ -89,7 +89,7 @@ function App() {
     <div>
       <div className="bg-neutral-900 text-white font-roboto text-base font-normal leading-6 text-left m-0 p-0">
         <div className="flex justify-between items-center py-4 px-6">
-          <a href='App.tsx' className="text-white font-bold text-2xl">MeTUBE</a>
+          <a href='App.tsx' className="text-white font-bold text-2xl bg-neutral-700 p-3 rounded-lg active:bg-neutral-800">MeTube</a>
           <div className="flex items-center">
             <input type="text" placeholder="Rechercher sur MeTube..." className='px-2 py-1 rounded-lg text-black mr-2' />
             <button className='bg-neutral-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg'>Rechercher</button>
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Tendances from './Tendances';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Tendances />
+    <App/>
   </React.StrictMode>
 );
