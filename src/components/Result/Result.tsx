c64a2bbc2dd077e16520f928235b890d5769a1f3
E.Delalande <delalande.ethan@gmail.com>
2023-04-12 11:15:49 +0200

    updated video on hover

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -29,10 +29,10 @@ const Result = (props: Props) => {
   }
   return (
 
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-        <a href="#">
-            <img className="rounded-t-lg max-h-80 max-w-xl hover:opacity-0" src={props.video.miniature} alt=""/>
-            <video muted className="rounded-t-lg max-h-80 max-w-xl" id="vidya" onMouseEnter={playVid} onMouseLeave={pauseVid} ref={videoRef}>
+    <div className="max-w-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a className="w-96 h-56 flex" href="#">
+            <img className="rounded-t-lg hover:opacity-0 w-96 h-56 absolute z-10" onMouseEnter={playVid} onMouseLeave={pauseVid} src={props.video.miniature} alt=""/>
+            <video muted className="rounded-t-lg w-80 w-96 h-56 absolute"ref={videoRef}>
               <source src={props.video.video} type='video/mp4'/>
             </video>
         </a>
