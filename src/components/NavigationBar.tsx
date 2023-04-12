ff1b6e2eb2b86dbd63b5dfe6367331862d03ed41
Ramjoh <romjah@orange.fr>
2023-04-12 12:04:27 +0200

    lien

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,17 +1,30 @@
+/* eslint-disable eol-last */
+/* eslint-disable import/no-duplicates */
+/* eslint-disable padded-blocks */
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
 /* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable @typescript-eslint/semi */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const NavigationBar = () => {
+
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+      navigate('/videopage');
+    };
   return (
     <nav className="bg-gray-800 py-2">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <a href="../pages/VideoPage.tsx">test</a>
             <div className="flex-shrink-0">
               <Link to="/">
                 <img
@@ -23,12 +36,11 @@ const NavigationBar = () => {
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
-                <Link
-                  to = "../pages/VideoPage.tsx"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
+              <div>
+                <div onClick={handleClick} style={{ cursor: 'pointer' }} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                   Video Editor
-                </Link>
+                </div>
+              </div>
 
                 <Link
                   to="/music"
@@ -62,4 +74,4 @@ const NavigationBar = () => {
   );
 };
 
-export default NavigationBar;
+export default NavigationBar
--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -2,12 +2,16 @@
 /* eslint-disable @typescript-eslint/space-before-function-paren */
 import React from 'react'
 import VideoEditor from '../components/VideoEditor'
+import NavigationBar from '../components/NavigationBar'
 
 function VideoPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="max-w-3xl">
-        <VideoEditor />
+    <div className="Video">
+      <NavigationBar />
+      <div className="flex justify-center items-center h-screen">
+        <div className="max-w-3xl">
+          <VideoEditor />
+        </div>
       </div>
     </div>
   )
