93f9fcc2378e8eb194ce7de8e189477563353420
YimoSsDev <116626946+YimoSsDev@users.noreply.github.com>
2023-04-12 13:35:33 +0200

    wait...

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -46,14 +46,21 @@ const NavigationBar = () => {
             </div>
           </div>
           <div className="-mr-2 flex md:hidden">
+            <label htmlFor="file-upload" className="cursor-pointer">
+              <span className="bg-blue-500 inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                Choose a file
+              </span>
+              <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+            </label>
             <button
               type="button"
-              className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="bg-blue-500 inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white ml-4"
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
-              <span className="sr-only">Open main menu</span>
-              <FaBars className="h-6 w-6" aria-hidden="true" />
+              <div className="spinner"></div>
+              Configure
+              <FaBars className="h-6 w-6 ml-2" aria-hidden="true" />
             </button>
           </div>
         </div>
