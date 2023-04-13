f0ba7cb4234ed9e227f6630e5e5ea4107e48d3ae
esteban dardilac <esteban.dardillac@gmail.com>
2023-04-13 12:50:51 +0200

    musique

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -1,46 +1,23 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 
 const MusicPlayer = () => {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const audioRef = useRef<HTMLAudioElement | null>(null);
+const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     setSelectedFile(file || null);
-
-    if (audioRef.current) {
-      audioRef.current.pause();
-      audioRef.current.load();
-    }
-  };
-
-  const handlePlay = () => {
-    if (audioRef.current) {
-      audioRef.current.play();
-    }
-  };
-
-  const handlePause = () => {
-    if (audioRef.current) {
-      audioRef.current.pause();
-    }
   };
 
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
       {selectedFile && (
-        <div>
-          <audio ref={audioRef} controls>
-            <source src={URL.createObjectURL(selectedFile)} />
-          </audio>
-          <button onClick={handlePlay}>Play</button>
-          <button onClick={handlePause}>Pause</button>
-        </div>
+        <audio controls>
+          <source src={URL.createObjectURL(selectedFile)} />
+        </audio>
       )}
     </div>
   );
 };
 
 export default MusicPlayer;
-
