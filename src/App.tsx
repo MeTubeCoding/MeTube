b5f88d8246eaed5d78d78d919e035a648d4fb991
Jojo12369 <jordan.busquet@gmail.com>
2023-04-13 16:06:04 +0200

    fin base historique

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css'
 
 function History() {
   return (
@@ -8,9 +7,9 @@ function History() {
         <img src="menu-btn.png" className="object-cover w-5 m-5"></img>
         <div className="rounded-full absolute top-0 left-0 right-0 bottom-0 hover:bg-white opacity-25 object-cover w-9 m-3"></div>
       </div>
-      <div className="bg-neutral-900 object-cover absolute top-14 left-0 bottom-0 w-60">
-
-      </div>
+      <div className="bg-neutral-900 object-cover absolute top-14 left-0 bottom-0 w-60"></div>
+      <div className="bg-neutral-900 object-cover absolute top-14 left-0 bottom-0 ml-60 w-20"></div>
+      <div className="bg-neutral-900 object-cover absolute top-14 right-0 bottom-0 ml-60 w-96"></div>
       <div className="bg-neutral-900 object-cover absolute top-14 left-80 bottom-0 right-0 w-60"></div>
       <div className="bg-neutral-900 object-cover absolute top-14 left-80 right-96 bottom-0 overflow-y-auto max-h-[1000px]">
         <p className="font-bold text-base text-white relative top-5">Historique de visionage</p>
@@ -31,7 +30,7 @@ function History() {
               [ Cover ] - Attack on Titan - FINAL SEASON (S4) OP - My War - David - Fils de Ange Momone - AM&D
             </div>
             <div className="absolute text-xs mr-56 left-72 top-12 mb-16 text-gray-400">
-            David fils de Momone  2,9 M de vues
+            David fils de Momone • 2,9 M de vues
             </div>
             <div className="absolute text-xs mr-24 left-72 top-20 text-gray-400">
               Mon dernier single est disponible absolument partout 
@@ -88,9 +87,6 @@ function History() {
             </div>
             <div className="bg-neutral-900 object-cover absolute top-28 ml-72 left-0 bottom-0 right-0"></div>
           </div>
-        </div>
-        <div>
-          <p className="font-bold text-base text-white relative mt-12 mb-6">Hier</p>
           <div className="relative mb-4 rounded-xl overflow-hidden">
             <iframe
               className="rounded-r-xl"
@@ -120,6 +116,233 @@ function History() {
 
               100 abonner: novembre 2022
               200 abonner: dimanche 26 mars 2023
+            </div>
+            <div className="bg-neutral-900 object-cover absolute top-28 ml-72 left-0 bottom-0 right-0"></div>
+          </div>
+        </div>
+        <div>
+          <p className="font-bold text-base text-white relative mt-12 mb-6">Hier</p>
+          <div className="relative mb-4 rounded-xl overflow-hidden">
+            <iframe
+              className="rounded-r-xl"
+              width="270"
+              height="150"
+              src="https://www.youtube.com/embed/4kMlLdX5lbw"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className="absolute text-base font-bold mr-56 left-72 top-0 bottom-0 text-white">
+              Finir Mario Bros DS sans MOURIR ?
+            </div>
+            <div className="absolute text-xs mr-56 left-72 top-12 mb-16 text-gray-400">
+              Lamatrak • 58 k vues
+            </div>
+            <div className="absolute text-xs mr-24 left-72 top-20 text-gray-400">
+              Si je meurs, je recommence tout le jeu ! C'est le défi que je me suis lancé aujourd'hui et que j'ai réalisé en live sur twitch: https://www.twitch.tv/lamatrak
+              Il s'agit de ma première vidéo Youtube, j'ai tout fait tout seul, de la minia au montage, n'hésitez pas à me dire ce que vous en pensez ! 
+              biz.
+            </div>
+            <div className="bg-neutral-900 object-cover absolute top-28 ml-72 left-0 bottom-0 right-0"></div>
+          </div>
+          <div className="relative mb-4 rounded-xl overflow-hidden">
+            <iframe
+              className="rounded-r-xl"
+              width="270"
+              height="150"
+              src="https://www.youtube.com/embed/Cp-n7RgynL4"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className="absolute text-base font-bold mr-56 left-72 top-0 bottom-0 text-white">
+              Ce Speedrunneur n'est Littéralement pas Humain...
+            </div>
+            <div className="absolute text-xs mr-56 left-72 top-12 mb-16 text-gray-400">
+              Julgane • 682 k vues
+            </div>
+            <div className="absolute text-xs mr-24 left-72 top-20 text-gray-400">
+              On regarde PLEINS de Records du Monde de Speedrun sur pleins de Jeux ! Y a d'ailleurs des ordinateurs qui jouent parfaitement, bref likez commentez si vous aimez svp.......
+
+              Liste des vidéos regardées: https://pastebin.com/CFiM8LFH
+
+              ▶Twitch : https://www.twitch.tv/julgane
+              ▶Discord : https://discord.com/invite/b2HzbF2
+              ▶Twitter : https://twitter.com/Julgane
+              ▶Speedrun : https://www.speedrun.com/user/Julgane
+
+              Montage par: https://twitter.com/Fast_z34 & Moi
+
+              #Julgane #Speedrun #React
+            </div>
+            <div className="bg-neutral-900 object-cover absolute top-28 ml-72 left-0 bottom-0 right-0"></div>
+          </div>
+          <div className="relative mb-4 rounded-xl overflow-hidden">
+            <iframe
+              className="rounded-r-xl"
+              width="270"
+              height="150"
+              src="https://www.youtube.com/embed/gMuPUsX3rU4"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className="absolute text-base font-bold mr-56 left-72 top-0 bottom-0 text-white">
+              5 épisodes LÉGENDAIRES des SIMPSON (à voir absolument) #2
+            </div>
+            <div className="absolute text-xs mr-56 left-72 top-12 mb-16 text-gray-400">
+              Cinéma Trash • 363 k vues
+            </div>
+            <div className="absolute text-xs mr-24 left-72 top-20 text-gray-400">
+              Est ce que vous connaissez vraiment tout des Simpson ?
+              La série va fêter son 750ème épisode, regarder les 34 saisons entier vous demanderait plus de 273 heures !
+              La naissance de Bart, le père secret de Lisa… il y a forcément des choses que vous avez manqué. Des épisodes légendaires, que vous devez              absolument aller voir !
+
+              Teromik : https://twitter.com/leTeromik
+              Mai-Lie : https://twitter.com/MaiLieBaelish
+              Nox : https://twitter.com/MrElNox
+
+              ▼▼ DÉROULE LA DESCRIPTION ▼▼
+              💃 TikTok  ► https://www.tiktok.com/@trasheurs
+              😂 Facebook ► https://www.facebook.com/trasheurs.vi...
+              👀 Instagram ► https://www.instagram.com/trash_neo_/
+              🎮 Discord ► https://discord.gg/YGAaDD6Cy2
+            </div>
+            <div className="bg-neutral-900 object-cover absolute top-28 ml-72 left-0 bottom-0 right-0"></div>
+          </div>
+          <div className="relative mb-4 rounded-xl overflow-hidden">
+            <iframe
+              className="rounded-r-xl"
+              width="270"
+              height="150"
+              src="https://www.youtube.com/embed/AHNzKWuUzxY"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className="absolute text-base font-bold mr-56 left-72 top-0 bottom-0 text-white">
+              TEST EN CARTON #110 - Hogwarts Legacy
+            </div>
+            <div className="absolute text-xs mr-56 left-72 top-12 mb-16 text-gray-400">
+              Savun • 302 k vues
+            </div>
+            <div className="absolute text-xs mr-24 left-72 top-20 text-gray-400">
+              - Chaîne Twitch : https://www.twitch.tv/savun
+              - Chaîne rediff VOD :   
+
+               / @vayke  
+              - S'abonner l Twitter : https://twitter.com/Savunew
+              - Facebook : https://www.facebook.com/Savun-211958...
+
+              Présentation de, apparemment, "l'exceptionnel Hogwarts Legacy : l'héritage de Poudlard" (spoiler : non) le jeu Harry Potter
+
+              Conditions de test :  version PS5 sur la PS5 
+
+              État de la run à la sortie du TEC :
+              - Aventure principale terminée 
+              - Jeu terminé 
+              - 29h de jeu 
+              - Jeu fait en mode Normal avec la maison Serpentard 
+              - 61% de Progression globale 
+              - 83% de Progression pour "Combat"
+              - 94% de Progression pour "Quêtes" 
+              - 19% de Progression pour "Exploration"
+              - 86% de Progression pour "Guide du Sorcier" 
+              - 25% de Progression pour "Salle sur Demande" 
+
+              --------------------------------------------------------------------------
+
+              Musiques utilisées par ordre d'apparition :
+
+              - Rugrats Intro / Razmoket thème 
+              - Joakim Karud - Take It Easy
+              - Banjo-Kazooie - Mumbo's Moutain 
+              - Joakim Karud - Waves (Thème de la chaîne) 
+              - Rayman 3 - Globox 
+              - Sonic Unleashed - Apotos Hub Day 
+              - Pokémon XD : Dr. Kaminko's Manor 
+              - Joakim Karud - Enjoy 
+              - Pokémon Colosseum - Normal Battle 
+              - Dark Souls 3 - Iudex Gundyr
+              - Lakey Inspired - Me 2 
+              - Okami - Agata Forest 
+              - Pokémon XD - ONBS 
+              - Harry Potter - Flute 
+              - Joakim Karud - Classic (outro de la chaîne) 
+
+              --------------------------------------------------------------------------
+
+              Crédits :
+
+              Le SoundCloud de Joakim Karud : https://soundcloud.com/joakimkarud
+
+              Le Youtube de Joakim Karud :   
+
+               / joakimkarud  
+
+              Le profil Artist Union de Joakim Karud : https://theartistunion.com/joakimkarud
+
+
+              --------------------------------------------------------------------------
+
+              Gameplay :
+
+              Gameplay de Hogwarts Legacy : l'Héritage de Poudlard par moi-même 
+
+              S.
+            </div>
+            <div className="bg-neutral-900 object-cover absolute top-28 ml-72 left-0 bottom-0 right-0"></div>
+          </div>
+          <div className="relative mb-4 rounded-xl overflow-hidden">
+            <iframe
+              className="rounded-r-xl"
+              width="270"
+              height="150"
+              src="https://www.youtube.com/embed/UobmLsDbiTg"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            <div className="absolute text-base font-bold mr-56 left-72 top-0 bottom-0 text-white">
+              Rocket League est le jeu le plus dur du monde.
+            </div>
+            <div className="absolute text-xs mr-56 left-72 top-12 mb-16 text-gray-400">
+              EGO • 534 k vues
+            </div>
+            <div className="absolute text-xs mr-24 left-72 top-20 text-gray-400">
+              On parle de LoL, on parle de Cs, on parle de Valo, mais tout le monde semble oublier Rocket League. Alors, moi, je vous explique.
+
+
+              _______________________________________________________
+                                                                                                                                  
+              Mes réseaux :
+
+              Twitch : https://www.twitch.tv/ego_
+              Twitter : https://twitter.com/rl_ego
+              TikTok : https://www.tiktok.com/@ego_rl1
+              Discord : https://discord.gg/             uufH7HXYh2                                                                                                                                
+              Mail : ego.pro003@gmail.com
+
+              _
+                                                                                                                                                                                    
+              Musiques d'Epidemic Sound :
+
+              1. Daytona - BLUE STEEL
+              2. Blue Dragon - Toby Tranter
+              3. Sticky Finger - Ballpoint
+                                                                                                                                   
+              _
+
+              Miniature par Nekatt : https://www.behance.net/jc161
+
+              _
+                                                                                                                                                                                    
+              #rocketleague  #rlcs  #esports
             </div>
             <div className="bg-neutral-900 object-cover absolute top-28 ml-72 left-0 bottom-0 right-0"></div>
           </div>
