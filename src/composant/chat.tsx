3f9e45b297d8581011bac47cd214f83f87271570
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-04-13 14:58:20 +0200

    ajout gitignore

--- src/composant/chat.tsx
+++ src/composant/chat.tsx
@@ -1,69 +1,69 @@
-import React from 'react';
-import { ModerationChat } from './moderationChat';
+import React from "react"
+import { ModerationChat } from "./moderationChat"
 
 export function Chat() {
-  function log() {
-    console.log('log');
-    const message = document.getElementById('message-input') as HTMLInputElement;
-    const local = {
-      message: message.value,
-      pseudo: 'Tristan',
-    };
-    fetch('http://127.0.0.1:5600/chat', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(local),
-    })
-      .then((res) => {
-        return res.text();
-      })
-      .then((res) => {
-        console.log(res);
-      });
-  }
+	function log() {
+		console.log("log")
+		const message = document.getElementById("message-input") as HTMLInputElement
+		const local = {
+			message: message.value,
+			pseudo: "Tristan",
+		}
+		fetch("http://127.0.0.1:5600/chat", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(local),
+		})
+			.then((res) => {
+				return res.text()
+			})
+			.then((res) => {
+				console.log(res)
+			})
+	}
 
-  function getChat() {
-    fetch('http://127.0.0.1:5600/chat', {
-      method: 'GET',
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        console.log(res);
-        const section = document.querySelector('section') as HTMLInputElement;
-        section.innerHTML = '';
-        res.forEach((message: { message: string; pseudo: string }) => {
-          const text = `${message.pseudo}: ${message.message}`;
-          const p = document.createElement('p');
-          p.textContent = text;
-          section.appendChild(p);
-        });
-      });
-  }
+	function getChat() {
+		fetch("http://127.0.0.1:5600/chat", {
+			method: "GET",
+		})
+			.then((res) => {
+				return res.json()
+			})
+			.then((res) => {
+				console.log(res)
+				const section = document.querySelector("section") as HTMLInputElement
+				section.innerHTML = ""
+				res.forEach((message: { message: string; pseudo: string }) => {
+					const text = `${message.pseudo}: ${message.message}`
+					const p = document.createElement("p")
+					p.textContent = text
+					section.appendChild(p)
+				})
+			})
+	}
 
-  return (
-    <>
-      <div className='height: 300px;overflow-y: scroll;border: 1px solid #ccc;padding: 10px;'>
-        <section></section>
+	return (
+		<>
+			<div className='height: 300px;overflow-y: scroll;border: 1px solid #ccc;padding: 10px;'>
+				<section></section>
 
-        <form className='margin-top: 10px;'>
-          <label htmlFor='message-input'>Message:</label>
-          <input
-            type='text'
-            name='message'
-            id='message-input'
-            className='width: 80%;padding: 10px;border: 1px solid #ccc;border-radius: 3px;'
-          ></input>
-          <p onClick={log}>send</p>
+				<form className='margin-top: 10px;'>
+					<label htmlFor='message-input'>Message:</label>
+					<input
+						type='text'
+						name='message'
+						id='message-input'
+						className='width: 80%;padding: 10px;border: 1px solid #ccc;border-radius: 3px;'
+					></input>
+					<p onClick={log}>send</p>
 
-          <p onClick={getChat}>chat</p>
-        </form>
-      </div>
+					<p onClick={getChat}>chat</p>
+				</form>
+			</div>
 
-      <ModerationChat />
-    </>
-  );
+			<ModerationChat />
+		</>
+	)
 }
