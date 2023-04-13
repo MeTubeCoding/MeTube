36e5964cf60bce4193e7423de3975bdce76674a4
Turbotard <turbotard9@gmail.com>
2023-04-13 10:20:21 +0200

    update eslint

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,28 +1,25 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React from 'react'
 import Result, { type IResult } from './Result/Result'
-export interface IResults extends Array<IResult> {
-
-}
+export interface IResults extends Array<IResult> {}
 
 interface Props {
   videos: IResults
 }
 
-const Results = (props: Props) => {
+const Results = (props: Props): any => {
   return (
     <>
-    {
-        props.videos.length === 0
-          ? <div></div>
-          : <div>
-            {
-                props.videos.map((video) => (
-                    <Result key={video.id} video={video}/>
-                ))
-            }
+      {props.videos.length === 0
+        ? (
+        <div></div>
+          )
+        : (
+        <div>
+          {props.videos.map(video => (
+            <Result key={video.id} video={video} />
+          ))}
         </div>
-    }
+          )}
     </>
   )
 }
