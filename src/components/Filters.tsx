7473ebd768f42f6dca79c2b87595fc2949860622
E.Delalande <delalande.ethan@gmail.com>
2023-04-13 14:33:39 +0200

    fixed config error

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -13,15 +13,37 @@ const Filters = () => {
     }
   }
 
+  const fVideo = () => {
+    console.log('filter by video')
+  }
+
+  const fChannel = () => {
+    console.log('filter by channel')
+  }
+
+  const fPlaylist = () => {
+    console.log('filter by playlist')
+  }
+
+  const fMovie = () => {
+    console.log('filter by movie')
+  }
+
   return (
-    <div className=" ml-72 text-me-yellow font-semibold">
+    <div className=" ml-64 text-me-yellow font-semibold">
       <button onClick={dropDown}>
         <p className="ml-5">Filtres</p>
       </button>
       <div ref={dropMenu} className="ml-5 hidden">
-        <a>Channels</a>
-        <a>Titles</a>
-        <a>Tags</a>
+        <p>
+          {' '}
+          Type
+          <hr className="w-10 m-3 border-me-yellow"></hr>
+          <a onClick={fVideo}>Video</a>
+          <a onClick={fChannel}>Channel</a>
+          <a onClick={fPlaylist}>Playlist</a>
+          <a onClick={fMovie}>Movie</a>
+        </p>
       </div>
       <hr className="w-9/12 m-3 border-me-yellow"></hr>
     </div>
