59f1ef3af56ee979e5bebe860415e1f188181ccb
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2023-04-17 16:41:11 +0200

    save 17h

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
-import WindowVideo from './pages/WindowVideo'
+import WindowUploadFusion from './pages/WindowUploadFusion'
 
 const App = () => {
   return (
-    <WindowVideo />
+    <WindowUploadFusion />
   )
 }
 
--- src/pages/WindowVideo.tsx
+++ src/pages/WindowVideo.tsx
@@ -1,33 +1,44 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 import 'tailwindcss/tailwind.css';
 
-const WindowVideo = () => {
+interface VideoPlayerProps {
+    videoId: string;
+}
 
-    const [videos, setVideos] = useState([])
-    const [comments, setComments] = useState([])
-    const [data, setData] = useState(null)
-
-    const { id } = useParams()
-
-    console.log(id)
+const WindowVideo: React.FC = () => {
     
     return (
 
-        <div className='py-20 px-9 bg-me-black flex flex-col h-full'>
-            <div className='left flex-1'>
-                <div className="flex justify-center">
-                    <iframe 
-                        src='https://www.youtube.com/watch?v=N-TCJquxeFk'
-                        title='Le meilleur duo évite la prison (ft Mister V, Léna, Theo Juice)'
-                        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                        allowFullScreen
-                    ></iframe>
+        <div className="bg-gray-100 min-h-screen">
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <div className="lg:flex lg:items-start lg:justify-between">
+                    <div className="flex-1 min-w-0">
+                        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+                            Titre de la vidéo
+                        </h2>
+                        <div className="flex mt-1">
+                            <span className="inline-flex items-center text-sm leading-5 font-medium text-gray-500 mr-6">
+                            1 234 vues
+                            </span>
+                            <span className="inline-flex items-center text-sm leading-5 font-medium text-gray-500 mr-6">
+                            Publié le 12 mars 2022
+                            </span>
+                        </div>
+                        <div className="mt-6">
+                            <div className="relative h-0 overflow-hidden pb-9/16">
+                                <iframe
+                                    className="absolute top-0 left-0 w-full h-full"
+                                    src="https://www.youtube.com/embed/N-TCJquxeFk"
+                                    allow="autoplay; encrypted-media"
+                                    allowFullScreen
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
     );
 };
 
