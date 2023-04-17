bf9ef993a71bc5b96f75efae3cbd5d23db65cd2d
Levaillant <89584966+Fl0-1@users.noreply.github.com>
2023-04-17 10:51:42 +0200

    backend

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,7 +4,6 @@ const app = express()
 const cors = require('cors')
 const bodyparser = require('body-parser')
 const path = require('path')
-const mongoose = 
 
 const { MongoClient, ServerApiVersion } = require('mongodb')
 
@@ -19,13 +18,8 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1
 })
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
 // Routes
+const channelsRoutes = require('./routes/channels.routes')
 
 var corsOptions = {
   origin: '*',
@@ -37,13 +31,13 @@ app.use(
   bodyparser.urlencoded({
     extended: true
   })
-  )
-  
-  app.use(cors(corsOptions))
-  app.use(express.json())
+)
 
-  app.use('/api/channels', channelsRoutes);
-  
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.use('/api/channels', channelsRoutes)
+
 app.post('/chat', (req, res) => {
   client.connect(err => {
     async function run() {
@@ -100,19 +94,19 @@ app.listen(5600, () => {
   console.log('Server app listening on port 5600')
 })
 
- app.post('/create-channel', (req, res) => {
-   client.connect(err => {
-     async function run() {
-       try {
-         const database = client.db('ChannelBDD')
-         const messages = database.collection('channel')
-         const query = req.body
-         await messages.insertOne(query)
-       } finally {
-         await client.close()
-       }
-     }
-     run().catch(console.dir)
-   })
-   res.end()
- })
+app.post('/create-channel', (req, res) => {
+  client.connect(err => {
+    async function run() {
+      try {
+        const database = client.db('ChannelBDD')
+        const messages = database.collection('channel')
+        const query = req.body
+        await messages.insertOne(query)
+      } finally {
+        await client.close()
+      }
+    }
+    run().catch(console.dir)
+  })
+  res.end()
+})
