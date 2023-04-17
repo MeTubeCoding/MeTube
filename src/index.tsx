9626a52ba6191dd49a3c17ae0db635feb6d35fd7
Stanewolfer <seiya0500@gmail.com>
2023-04-17 15:28:26 +0200

    changement de tendances pr éviter conflits

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Tendances from './Tendances';
+import Tendances from './Tendances_mocking';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
