531bc4af3cd717d42d2547e5a60fc172d6df4042
Adilina04 <113524418+Adilina04@users.noreply.github.com>
2023-04-17 10:51:14 +0200

    delete react-ticker

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,20 @@
-/* eslint-disable eol-last */
-/* eslint-disable multiline-ternary */
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-
-import React from 'react'
-import NavigationBar from '../components/NavigationBar'
+import React from "react";
+import NavigationBar from "../components/NavigationBar";
 
 const HomePage = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: 'calc(100vh - 60px)' }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "calc(100vh - 60px)",
+      }}
+    >
       <NavigationBar />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -1,22 +1,16 @@
-/* eslint-disable @typescript-eslint/key-spacing */
-/* eslint-disable eol-last */
-/* eslint-disable multiline-ternary */
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-
-import React, { useState } from 'react'
-import VideoItem from '../components/VideoItem'
-import VideoEditor from '../components/VideoEditor'
-import NavigationBar from '../components/NavigationBar'
+import React, { useState } from "react";
+import VideoItem from "../components/VideoItem";
+import VideoEditor from "../components/VideoEditor";
+import NavigationBar from "../components/NavigationBar";
 
 const VideoPage = () => {
-  const [selectedVideo, setSelectedVideo] = useState<File | null>(null)
+  const [selectedVideo, setSelectedVideo] = useState<File | null>(null);
 
   const handleVideoUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
-      setSelectedVideo(event.target.files[0])
+      setSelectedVideo(event.target.files[0]);
     }
-  }
+  };
 
   return (
     <div className="flex flex-col h-screen">
@@ -27,11 +21,22 @@ const VideoPage = () => {
             <VideoItem video={selectedVideo} />
           ) : (
             <div className="flex flex-col items-center justify-center space-y-4">
-              <h2 className="text-xl font-medium text-gray-500">Select a video to get started:</h2>
-              <label htmlFor="video-upload" className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md cursor-pointer hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              <h2 className="text-xl font-medium text-gray-500">
+                Select a video to get started:
+              </h2>
+              <label
+                htmlFor="video-upload"
+                className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md cursor-pointer hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
                 Choose a file
               </label>
-              <input id="video-upload" type="file" accept="video/*" onChange={handleVideoUpload} className="sr-only" />
+              <input
+                id="video-upload"
+                type="file"
+                accept="video/*"
+                onChange={handleVideoUpload}
+                className="sr-only"
+              />
             </div>
           )}
         </div>
@@ -42,7 +47,7 @@ const VideoPage = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoPage
+export default VideoPage;
