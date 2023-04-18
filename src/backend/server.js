558ba7657ee367bf2464edd12955ba24c13c49bb
E.Delalande <delalande.ethan@gmail.com>
2023-04-18 15:02:47 +0200

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -214,102 +214,36 @@ app.post('/channels', function (req, res) {
 })
 
 app.post('/videos', function (req, res) {
-  const fakeVideos = [
-    {
-      id: 1,
-      title: 'Nabil a cassé mon bong',
-      miniature:
-        'https://imgs.search.brave.com/5KvnUyLxAcJHuuU_Ry7pJksq9llJ1Cf0XXfyuKJ7IM0/rs:fit:1200:900:1/g:ce/aHR0cDovL2kuaW1n/dXIuY29tL3ExMmgy/LmpwZw',
-      channel: 'Roro',
-      video:
-        'https://cdn.discordapp.com/attachments/935989994735169546/1082443934741053530/redditsave.com_real_hol_up-pvq9he9jok571.mp4',
-      tags: ['défonce', 'réaction'],
-      views: 10,
-      release: '2022-03-23',
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      short: false,
-      duration: ''
-    },
-    {
-      id: 2,
-      title: 'Gros bartsimpson avec Narbok',
-      miniature:
-        'https://imgs.search.brave.com/FjKYVIUEMX-Rtp38q3Ztm3a7j6bsX5rOpQGK5BGms5g/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDgxMTc0/OTcucG5n',
-      channel: 'Ludwig',
-      video:
-        'https://cdn.discordapp.com/attachments/935989994735169546/1082443934741053530/redditsave.com_real_hol_up-pvq9he9jok571.mp4',
-      tags: ['défonce', 'macron'],
-      views: 10,
-      release: '2022-03-23',
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      short: false,
-      duration: ''
-    },
-    {
-      id: 3,
-      title: 'Nabil est parti sans fumer...',
-      miniature:
-        'https://imgs.search.brave.com/H4X-HS4LrQqKVL9iot-eS6yt_uyWTNvu-KZfjO_i9RI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9oaWdo/dGltZXMuY29tL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE3LzAx/L0xvdHNPZkpvaW50/cy5qcGc',
-      channel: 'Maxime',
-      video:
-        'https://cdn.discordapp.com/attachments/935989994735169546/1082443934741053530/redditsave.com_real_hol_up-pvq9he9jok571.mp4',
-      tags: ['défonce'],
-      views: 10,
-      release: '2022-03-23',
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      short: false,
-      duration: ''
-    },
-    {
-      id: 4,
-      title: 'Il a mangé tout le tramadole omg !',
-      miniature:
-        'https://imgs.search.brave.com/sQassPoRQw3-kmZKo4fGSGpSGyCxjdlyDQmobfn-YYY/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/cHVibGljZG9tYWlu/cGljdHVyZXMubmV0/L3BpY3R1cmVzLzQw/MDAwL3ZlbGthL2xl/bnRpbGt5LmpwZw',
-      channel: 'Roro',
-      video:
-        'https://cdn.discordapp.com/attachments/935989994735169546/1082443934741053530/redditsave.com_real_hol_up-pvq9he9jok571.mp4',
-      tags: ['défonce', 'macron', 'fun', 'réaction'],
-      views: 10,
-      release: '2022-03-23',
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      short: false,
-      duration: ''
-    },
-    {
-      id: 5,
-      title: 'Il a une calvitie',
-      miniature:
-        'https://imgs.search.brave.com/Mzd1G1UAR4KtlSpOFaL5bLw8jY4YabGntaZq_3qM78Y/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4w/U0hscC16THhYTmg1/a29hUFhHdk9RSGFI/YSZwaWQ9QXBp',
-      channel: 'Roro',
-      video:
-        'https://cdn.discordapp.com/attachments/935989994735169546/1082443934741053530/redditsave.com_real_hol_up-pvq9he9jok571.mp4',
-      tags: ['macron'],
-      views: 10,
-      release: '2022-03-23',
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      short: true,
-      duration: ''
+    client.connect(err => {
+      async function run() {
+        try {
+          const database = client.db('ChannelBDD')
+          const videos = database.collection('videos')
+          const query = req.body
+          const video = await videos.find({ videos: query })
+          console.log(video)
+          const requestString = req.body.data
+          const regexString = requestString.replace(/ /g, '|').split('').join('.*')
+          const regex = new RegExp(regexString, 'i')
+          const requestedVideos = video.filter(
+            video =>
+            regex.test(video.title) ||
+            regex.test(video.channel) ||
+            video.tags.some(tag => regex.test(tag))
+          )
+          if (requestedVideos.length > 0) {
+            res.json(requestedVideos)
     }
-  ]
+        } finally {
+          // Ensures that the client will close when you finish/error
+          await client.close()
+        }
+      }
+      run().catch(console.dir)
+    })
 
-  const requestString = req.body.data
-  const regexString = requestString.replace(/ /g, '|').split('').join('.*')
-  const regex = new RegExp(regexString, 'i')
-  const requestedVideos = fakeVideos.filter(
-    video =>
-      regex.test(video.title) ||
-      regex.test(video.channel) ||
-      video.tags.some(tag => regex.test(tag))
-  )
-
-  if (requestedVideos.length > 0) {
-    res.json(requestedVideos)
-  }
+    
+  
 })
 
 // Optionnel a vous de voir pour vous adapter à votre problématique :
