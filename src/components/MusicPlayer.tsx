b5a3f341194fb6b0b60b53659290edde16888d28
esteban dardilac <esteban.dardillac@gmail.com>
2023-04-18 09:58:03 +0200

    on teste des trucs pour la musique

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -1,23 +1,22 @@
 import React, { useState, useRef } from 'react'
-import  VideoPageProps  from '../pages/VideoPage';
+import PlayMusicWithVideo from './PlayMusicWithVideo';
 
 
 
-interface Props {
-  playMusicWithVideo: (audioFile: File) => void;
-}
 
-const MusicPlayer = (props : Props) => {
+
+const MusicPlayer = () => {
   const [selectedAudios, setSelectedAudios] = useState<File[]>([])
   const audioRefs = useRef<Array<HTMLAudioElement | null>>([])
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleMusicChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files
     if (files != null) {
       const fileList = Array.from(files)
       setSelectedAudios([...selectedAudios, ...fileList])
     }
   }
+  
 
 
   // const handlePlay = (index: number) => {
@@ -39,15 +38,23 @@ const MusicPlayer = (props : Props) => {
     audioRefs.current.splice(index, 1)
   }
 
+  const handlePlayWithVideo = () => {
+    if (audioRefs.current.length > 0 && selectedAudios.length > 0) {
+      PlayMusicWithVideo(selectedAudios[selectedAudios.length - 1]) // Récupérer le dernier fichier audio ajouté
+    }
+  }
+
   return (
     <div>
-      <input type="file" onChange={handleFileChange} multiple />
+      <input type="file" onChange={handleMusicChange} multiple />
       {selectedAudios.map((file, index) => (
         <div key={index}>
           <audio ref={(ref) => (audioRefs.current[index] = ref)} controls>
             <source src={URL.createObjectURL(file)} />
           </audio>
-          <button onClick={() => { handleRemove(index) }}>Remove</button>
+          <button onClick={() =>{handlePlayWithVideo}}>Jouer la musique sur la vidéo</button>
+          <br></br>
+          <button onClick={() => { handleRemove(index) }}>Enlever la musique</button>
         </div>
       ))}
     </div>
--- src/components/PlayMusicWithVideo.tsx
+++ src/components/PlayMusicWithVideo.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import VideoPage from '../pages/VideoPage'
-import MusicPlayer from './MusicPlayer'
+import { audioRefs, selectedAudios } from './MusicPlayer'
 
 interface musicProps{
-
+  handleMusicChange : (file: File) => void;
 }
 
 interface videoProps{
-    selectedVideo
+  handleVideoUpload : (file: File) => void;
 }
 
-const PlayMusicWithVideo = () => {
+const PlayMusicWithVideo = (audioFile: File) => {
+    const videoElement = document.getElementById("video-element") as HTMLVideoElement; 
+    if (audioRefs.current.length > 0 && selectedAudios.length > 0 && videoElement) {
+      const audioElement = audioRefs.current[audioRefs.current.length - 1] 
+      audioElement.src = URL.createObjectURL(audioFile); 
+      audioElement.play(); 
+      videoElement.addEventListener("play", () => {
+        if (audioElement) {
+          audioElement.play(); 
+        }
+      });
+      videoElement.addEventListener("pause", () => {
+        if (audioElement) {
+          audioElement.pause(); 
+        }
+      });
+      videoElement.addEventListener("ended", () => {
+        if (audioElement) {
+          audioElement.pause();
+        }
+      });
+    }
 
+  
 
 
   return (
