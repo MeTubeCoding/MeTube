c0165e7b782cdcc269f3ad82671db18887581a9e
Turbotard <turbotard9@gmail.com>
2023-04-20 14:21:03 +0200

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,11 @@ import History from './pages/feed/history'
 import Bibliotheque from './pages/feed/library'
 import Abonnements from './pages/feed/subscriptions'
 import Live from './composant/Live'
+import ChaineAccueil from './components/ChaineAccueil'
+import WindowUpload from './pages/WindowUpload'
+import WindowVideo from './pages/WindowVideo'
+import WindowCreateChannel from './pages/WindowCreateChannel'
+import WindowCreateOneChannel from './pages/WindowCreateOneChannel'
 
 function App() {
   return (
@@ -30,6 +35,11 @@ function App() {
             <Route path="/feed/library" element={<Bibliotheque />} />
             <Route path="/feed/subscriptions" element={<Abonnements />} />
             <Route path="/live" element={<Live />} />
+            <Route path="/chaine-accueil" element={<ChaineAccueil />} />
+            <Route path="/window-upload" element={<WindowUpload />} />
+            <Route path="/window-video" element={<WindowVideo />} />
+            <Route path="/window-create-channel" element={<WindowCreateChannel />} />
+            <Route path="/window-create-one-channel" element={<WindowCreateOneChannel />} />
           </Routes>
         </BrowserRouter>
       </div>
