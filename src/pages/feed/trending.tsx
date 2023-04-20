a49184586335fad53b9c29438fa75237c167b950
Stanewolfer <seiya0500@gmail.com>
2023-04-20 11:15:42 +0200

    Update trending.tsx

--- src/pages/feed/trending.tsx
+++ src/pages/feed/trending.tsx
@@ -16,20 +16,20 @@ import {
 
 function Tendances() {
   return (
-    <div className="h-full text-me-yellow bg-me-darkpurple font-sans-roboto font-normal text-base leading-relaxed text-left m-0 p-0 h-screen">
-      <div className="flex flex-row justify-center h-screen bg-neutral-900 bg-me-darkpurple">
+    <div className="h-full text-me-yellow font-sans-roboto font-normal text-base leading-relaxed text-left m-0 p-0 h-screen">
+      <div className="flex flex-row justify-center h-screen bg-neutral-900  ">
         <div className="bg-neutral-700 px-8 mr-20 "></div>
         <div>
-          <div className="mb-5 flex flex-row justify-center items-center my-3 mx-1/100 lg:mx-5/100 bg-me-darkpurple">
+          <div className="mb-5 flex flex-row justify-center items-center my-3 mx-1/100 lg:mx-5/100  ">
             <img
               className="rounded-full mr-2%"
               src="https://www.youtube.com/img/trending/avatar/trending.png"
             ></img>
-            <h3 className="ml-5 p-0 text-center leading-normal text-3xl bg-me-darkpurple">
+            <h3 className="ml-5 p-0 text-center leading-normal text-3xl  ">
               TRENDINGS
             </h3>
           </div>
-          <div className="w-full ml-5 flex flex_col justify-center bg-me-darkpurple">
+          <div className="w-full ml-5 flex flex_col justify-center  ">
             {trend_new()}
             {trend_music()}
             {trend_gaming()}
