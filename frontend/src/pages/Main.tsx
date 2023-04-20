73ffa44465684ff045ca06916f778c164d35628c
E.Delalande <delalande.ethan@gmail.com>
2023-04-20 09:29:24 +0200

    Update Main.tsx

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,48 +1,18 @@
 import React, { useState } from 'react'
-import Results from '../components/Results'
-import Navbar from '../components/Navbar'
 import { useOnSearch } from '../components/useOnSearch'
+import Navbar from '../components/Navbar'
 import SideBar from '../components/SideBar'
-import Filters from '../components/Filters'
-import { channel } from 'diagnostics_channel'
-import { set } from 'mongoose'
+import Recherche from './Recherche'
+import Tendances from './feed/trending'
+import Results from '../components/Results'
 
 const Main = () => {
+  const [HasSearched, setHasSearched] = useState(false)
+
   const [isSideBarVisible, setIsSideBarVisible] = useState(false)
   const [filter, setFilter] = useState('none')
+  const [sortBy, setSort] = useState('none')
   const { videos, channels, onSearch } = useOnSearch()
-
-  const filterChannel = () => {
-    if (filter != 'channel') {
-      setFilter('channel')
-    } else {
-      setFilter('none')
-    }
-  }
-
-  const filterMovie = () => {
-    if (filter != 'movie') {
-      setFilter('movie')
-    } else {
-      setFilter('none')
-    }
-  }
-
-  const filterVideo = () => {
-    if (filter != 'video') {
-      setFilter('video')
-    } else {
-      setFilter('none')
-    }
-  }
-
-  const filterPlaylist = () => {
-    if (filter != 'playlist') {
-      setFilter('playlist')
-    } else {
-      setFilter('none')
-    }
-  }
 
   const toggleSideBarVisibility = () => {
     setIsSideBarVisible(prevState => !prevState)
@@ -51,24 +21,34 @@ const Main = () => {
   return (
     <div className="max-h-screen">
       <div style={{ height: '8.5vh' }}>
-        <Navbar onSearch={onSearch} onToggleSideBar={toggleSideBarVisibility} />
+        <Navbar
+          setSearched={setHasSearched}
+          onSearch={onSearch}
+          onToggleSideBar={toggleSideBarVisibility}
+        />
       </div>
       <div className="flex flex-col" style={{ height: '92.5vh' }}>
         <SideBar visible={isSideBarVisible} />
-        <Filters
-          visible={isSideBarVisible}
-          filterChannel={filterChannel}
-          filterVideo={filterVideo}
-          filterMovie={filterMovie}
-          filterPlaylist={filterPlaylist}
-          filter={filter}
-        ></Filters>
-        <Results
-          visible={isSideBarVisible}
-          filter={filter}
-          videos={videos}
-          channels={channels}
-        />
+        {HasSearched ? (
+          <div>
+            <Recherche
+              filter={filter}
+              sortBy={sortBy}
+              visible={isSideBarVisible}
+              setFilter={setFilter}
+              setSort={setSort}
+            />
+            <Results
+              sortBy={sortBy}
+              videos={videos}
+              channels={channels}
+              filter={filter}
+              visible={isSideBarVisible}
+            ></Results>
+          </div>
+        ) : (
+          <Tendances></Tendances>
+        )}
       </div>
     </div>
   )
