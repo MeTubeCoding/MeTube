29e97897a51b48063a501bb33ba4c383cc81b563
Sacha <sacha.harel@gmail.com>
2023-04-20 15:00:46 +0200

    update trending.tsx, mocking.json and trending_categories.tsx

--- frontend/src/timeline_components/trending_categories.tsx
+++ frontend/src/timeline_components/trending_categories.tsx
@@ -3,56 +3,63 @@ import React from 'react'
 import mockData from './mocking.json'
 
 export function trend_tag() {
-  const chaine = mockData.chaine;
-  const videos = mockData.videos;
-  const vid_a_afficher = [];
-  
-    for (let i = 0; i < chaine.length; i++) {
-      const trend_tag = mockData.chaine[i].tag_pref;
-  
-      for (let j = 0; j < videos.length; j++) {
-        const videos_tags = mockData.videos[j].tags;
-        let tag_found = false;
-  
-        for (let k = 0; k < videos_tags.length; k++) {
-          if (trend_tag.includes(videos_tags[k])) {
-            tag_found = true;
-            break;
-          }
-        }
-  
-        if (tag_found) {
-          vid_a_afficher.push(videos[j]);
+  const chaine = mockData.chaine
+  const videos = mockData.videos
+  const vid_a_afficher = []
+
+  for (let i = 0; i < chaine.length; i++) {
+    const trend_tag = mockData.chaine[i].tag_pref
+
+    for (let j = 0; j < videos.length; j++) {
+      const videos_tags = mockData.videos[j].tags
+      let tag_found = false
+
+      for (let k = 0; k < videos_tags.length; k++) {
+        if (trend_tag.includes(videos_tags[k])) {
+          tag_found = true
+          break
         }
       }
+
+      if (tag_found) {
+        vid_a_afficher.push(videos[j])
+      }
     }
-  
-    // Afficher les vidéos ayant des tags en commun avec les chaînes
-    return (
+  }
+
+  // Afficher les vidéos ayant des tags en commun avec les chaînes
+  return (
+    <div>
       <div>
-        <div>
-          {vid_a_afficher.map(video => (
-            <div className='flex mb-[5%]'>
-              <div className='w-1/3'>
-                <img src={video.minia} />
+        {vid_a_afficher.map(video => (
+          <div className="flex mb-[5%] text-me-yellow">
+            <div className="w-1/3">
+              <img src={video.minia} />
+            </div>
+            <div className="w-2/3 px-4">
+              <h5 className="text-lg font-bold">{video.titre}</h5>
+              <div className="flex flex-row">
+                <img className="w-6 h-6 mr-2 rounded-full" src={video.img_chaine} />
+                <p className="text-gray-500">{video.chaine}</p>
               </div>
-              <div className='w-2/3 px-4'>
-                <h5 className='text-lg font-bold'>{video.titre}</h5>
-                <p className='text-gray-500'>{video.chaine}</p>
-                <p className='text-gray-500'>{video.tags[0]}</p>
+              <div className="flex flex-row">
+                {video.tags.map((tag, index) => (
+                  <p key={index} className="text-gray-500 mr-1">
+                    #{tag}
+                  </p>
+                ))}
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  )
+}
 
 // OBSOLETE =======================================================================================
 // ================================================================================================
 // ================================================================================================
-
-
 
 export function trend_videos() {
   const trend_new = mockData.videos
